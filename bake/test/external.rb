4ae5c147b3a26918437f83cf773fec9ed0fc376c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-08-28 17:00:25 +1200

    Support extra lines in gemfile.

--- bake/test/external.rb
+++ bake/test/external.rb
@@ -8,20 +8,17 @@ DEFAULT_COMMAND = "bake test"
 def external(input: nil, gemspec: self.find_gemspec)
 	require 'bundler'
 	require 'yaml'
-
+	
 	if input.nil? and File.exist?(DEFAULT_EXTERNALS_PATH)
 		# symbolize_names is unsupported on Ruby 2.7
 		input = YAML.load_file(DEFAULT_EXTERNALS_PATH) # , symbolize_names: true)
 	end
-
-	input.each do |key, config|
-		config.transform_keys!(&:to_sym);
-
-		url = config[:url]
-		command = config.fetch(:command, DEFAULT_COMMAND)
-
+	
+	input&.each do |key, config|
+		config = config.transform_keys(&:to_sym)
+		
 		Bundler.with_unbundled_env do
-			clone_and_test(gemspec.name, key, url, command)
+			clone_and_test(gemspec.name, key, config)
 		end
 	end
 end
@@ -40,15 +37,18 @@ def find_gemspec(glob = "*.gemspec")
 	end
 end
 
-def clone_and_test(name, key, url, command)
+def clone_and_test(name, key, config)
 	require 'fileutils'
+	
+	url = config[:url]
+	command = config.fetch(:command, DEFAULT_COMMAND)
 	
 	path = "external/#{key}"
 	
 	unless File.directory?(path)
 		FileUtils.mkdir_p path
 		system("git", "clone", "--depth", "1", url, path)
-
+		
 		# I tried using `bundle config --local local.async ../` but it simply doesn't work.
 		# system("bundle", "config", "--local", "local.async", __dir__, chdir: path)
 		
@@ -58,10 +58,14 @@ def clone_and_test(name, key, url, command)
 		File.open(gemfile_path, "a") do |file| 
 			file.puts nil, "# Added by external testing:"
 			file.puts("gem #{name.to_s.dump}, path: '../../'")
+			
+			config[:extra]&.each do |line|
+				file.puts(line)
+			end
 		end
-
+		
 		system("bundle", "install", chdir: path)
 	end
-
+	
 	system(*command, chdir: path) or abort("External tests #{key} failed!")
 end
